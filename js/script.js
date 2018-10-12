4f9d9e5e2139e9173667978d6edb44a548e4a4f9
tobyornottobe <straurum@gmx.ch>
2018-10-12 21:57:07 +0200

    added rating stars

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function parallax() {
   var wScroll = $(window).scrollTop();
 
   $('.parallax--bg').css('background-position',
-  'center '+(wScroll*0.25)+'px')
+  'center '+(wScroll*0.75)+'px');
 
 }
 
@@ -18,14 +18,14 @@ function parallax() {
 //drag and drop
 var adjustment;
 
-$("ul").sortable({
+$(".portfolio").sortable({
   group: 'simple_with_animation',
   pullPlaceholder: false,
   // animation on drop
   onDrop: function  ($item, container, _super) {
-    var $clonedItem = $('<li/>').css({height: 0});
+    var $clonedItem = $("<li/>").css({height: 0});
     $item.before($clonedItem);
-    $clonedItem.animate({'height': $item.height()});
+    $clonedItem.animate({"height": $item.height()});
 
     $item.animate($clonedItem.position(), function  () {
       $clonedItem.detach();
@@ -52,3 +52,66 @@ $("ul").sortable({
     });
   }
 });
+
+
+
+//Star Rating
+
+$(document).ready(function(){
+
+  /* 1. Visualizing things on Hover - See next part for action on click */
+  $('.stars li').on('mouseover', function(){
+    var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
+
+    // Now highlight all the stars that's not after the current hovered star
+    $(this).parent().children('li.star').each(function(e){
+      if (e < onStar) {
+        $(this).addClass('hover');
+      }
+      else {
+        $(this).removeClass('hover');
+      }
+    });
+
+  }).on('mouseout', function(){
+    $(this).parent().children('li.star').each(function(e){
+      $(this).removeClass('hover');
+    });
+  });
+
+
+  /* 2. Action to perform on click */
+  $('.stars li').on('click', function(){
+    var onStar = parseInt($(this).data('value'), 10); // The star currently selected
+    var stars = $(this).parent().children('li.star');
+
+    for (i = 0; i < stars.length; i++) {
+      $(stars[i]).removeClass('selected');
+    }
+
+    for (i = 0; i < onStar; i++) {
+      $(stars[i]).addClass('selected');
+    }
+
+/*
+    // JUST RESPONSE (Not needed)
+    var ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
+    var msg = "";
+    if (ratingValue > 1) {
+        msg = "Thanks! You rated this " + ratingValue + " stars.";
+    }
+    else {
+        msg = "We will improve ourselves. You rated this " + ratingValue + " stars.";
+    }
+    responseMessage(msg);
+*/
+
+  });
+
+});
+
+/*
+function responseMessage(msg) {
+  $('.success-box').fadeIn(200);
+  $('.success-box div.text-message').html("<span>" + msg + "</span>");
+}*/
